4fd73d41d10f9612a30d37da42425ecd6c52eab7
Syed Hamza <SyedHamza0196@users.noreply.github.com>
2021-12-21 15:05:45 +0500

    organized print statements

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from moviepy.video.VideoClip import VideoClip
 from pydub import AudioSegment
 from textblob import TextBlob
 
-st.title("Video to Text and translation")
+st.title("Video --> Text --> Urdu Translation")
 st.write()
 
 subscription_key = 'be934929aaa74b068f1aa6ec0e32e3f8'
@@ -16,11 +16,9 @@ headers = {
     'Ocp-Apim-Subscription-Key': 'be934929aaa74b068f1aa6ec0e32e3f8',
 }
 
-st.write("uploading file")
-uploaded_file = st.file_uploader("Choose an audio...")
-
+uploaded_file = st.file_uploader("Choose an mp4 video file...")
+st.header("Processing Video")
 if uploaded_file is not None:
-    print("type(uploaded_file) --> ", type(uploaded_file))
     with open("sample.mp4", "wb") as f:
         f.write(uploaded_file.getbuffer())
         video = VideoFileClip("sample.mp4")
@@ -28,17 +26,18 @@ if uploaded_file is not None:
         video.audio.write_audiofile("sample.mp3")
         sound = AudioSegment.from_mp3("sample.mp3")
         sound.export("sample.wav", format="wav")
+        st.write("Format conversion successful!")
 
     with open("sample.wav", 'rb') as payload:
-        st.write("Processing audio")
+        st.header("Processing Audio")
         response = requests.request("POST", url, headers=headers, data=payload)
         long_text = response.text.split('DisplayText":')[
             1].split(',"Offset')[0]
-        st.write("Speech To Text Result")
+        st.subheader("Speech To Text Result")
         st.write(long_text)
 
-        st.write("Translating to english")
+        st.header("Translating to english")
         blob_ = TextBlob(long_text)
         output = blob_.translate(to='ur')
-        st.write("Translation Result")
+        st.subheader("Translation Result")
         st.write(output)
